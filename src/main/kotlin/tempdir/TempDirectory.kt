b91c438bdcf91afd7b64aca44aeaf5712f0186e9
Sander <sanderkrabbenborg@hotmail.com>
2022-08-23 00:14:07 +0200

    Fix folder structure

--- src/main/kotlin/tempdir/TempDirectory.kt
+++ src/main/kotlin/tempdir/TempDirectory.kt
@@ -4,22 +4,22 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
 
-private const val TEMP_PATH_PREFIX = "TempDirectory-"
-private const val SUB_PATH_PREFIX = "SubDirectory-"
+private const val TEMP_DIR_PREFIX = "TempDirectory-"
+private const val SUB_DIR_PREFIX = "SubDirectory-"
 
 class TempDirectory internal constructor() : AutoCloseable {
 
-    private val baseDir: Path = Files.createTempDirectory(TEMP_PATH_PREFIX)
+    private val baseDir: Path = Files.createTempDirectory(TEMP_DIR_PREFIX)
 
     /**
      * Creates a new subfolder in this [TempDirectory]. These folders are guaranteed to be empty.
      */
-    fun newSubDir(): Path = Files.createDirectories(baseDir.resolve(SUB_PATH_PREFIX + UUID.randomUUID()))
+    fun newSubDir(): Path = Files.createDirectories(baseDir.resolve(SUB_DIR_PREFIX + UUID.randomUUID()))
 
     /**
      * Closes the current [TempDirectory] by deleting all the files and folders stored in the directory it represents.
-     * Since [TempDirectory] operates on an OS temporary folder, the folder it represents will be cleared upon OS
-     * shutdown. Therefore, it ignored any errors upon closing.
+     * Since [TempDirectory] operates on an OS temporary folder, the folder it represents will also be cleared upon OS
+     * shutdown. Therefore, it ignores any errors upon closing.
      */
     override fun close() {
         runCatching {
